721eb7f7127edc16d5e17c28eb5d2de6363ede4a
Alejandro Melo Domínguez <alejandroivan@icloud.com>
2019-07-27 22:01:52 -0400

    Initial scene for Search.

---
+++ itunesapi_csTests/Scenes/Search/Spies/SearchDisplayLogicSpy.swift
@@ -0,0 +1,13 @@
+//
+//  SearchDisplayLogicSpy.swift
+//  itunesapi_csTests
+//
+//  Created by Alejandro Melo Domínguez on 7/27/19.
+//  Copyright © 2019 Alejandro Melo Domínguez. All rights reserved.
+//
+
+@testable import itunesapi_cs
+import XCTest
+
+class SearchDisplayLogicSpy: SearchDisplayLogic {
+}
