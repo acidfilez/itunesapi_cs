d65bec8f6634972c232400425d13c65f94cab6c6
Alejandro Melo Domínguez <alejandroivan@icloud.com>
2019-07-29 22:17:18 -0400

    didSelectMedia on presenter

--- itunesapi_csTests/Scenes/Details/DetailsInteractorTests.swift
+++ itunesapi_csTests/Scenes/Details/DetailsInteractorTests.swift
@@ -92,4 +92,29 @@ class DetailsInteractorTests: XCTestCase {
         XCTAssertFalse(presenterSpy.presentAlbumDetailsCalled)
         XCTAssertTrue(presenterSpy.presentAlbumDetailsErrorMessageCalled)
     }
+
+    func testDidSelectMedia() {
+        // Given
+        let media = Media(
+            wrapperType: "track",
+            artistName: "Various Artists",
+            collectionId: 1,
+            collectionName: "The Lion King",
+            kind: "soundtrack",
+            trackId: 1,
+            trackName: "Circle of Life",
+            trackNumber: 1,
+            artwork: "https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/59/f2/cb/59f2cbb4-6800-2298-8de5-070caae3ec6a/00050086085873.rgb.jpg/100x100w.jpg",
+            previewUrl: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/53/bc/1c/53bc1c62-87a6-b255-7888-62e00972b18e/mzaf_1500676314994411111.plus.aac.p.m4a"
+        )
+        let presenterSpy = DetailsPresentationLogicSpy()
+        sut.presenter = presenterSpy
+        let request = Details.Request(media: media)
+
+        // When
+        sut.didSelectMedia(request: request)
+
+        // Then
+        XCTAssertTrue(presenterSpy.presentMediaPlayerCalled)
+    }
 }
