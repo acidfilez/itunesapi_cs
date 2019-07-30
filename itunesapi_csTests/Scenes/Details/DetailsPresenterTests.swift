785702157d59e03e19ffd9e780fc77a1e3dea5ce
Alejandro Melo Domínguez <alejandroivan@icloud.com>
2019-07-29 22:29:26 -0400

    Route to media player broken test.

--- itunesapi_csTests/Scenes/Details/DetailsPresenterTests.swift
+++ itunesapi_csTests/Scenes/Details/DetailsPresenterTests.swift
@@ -64,6 +64,27 @@ class DetailsPresenterTests: XCTestCase {
     }
 
     func testPresentMediaPlayer() {
-        
+        // Given
+        let media = Media(
+            wrapperType: "track",
+            artistName: "Various Artists",
+            collectionId: 1,
+            collectionName: "The Lion King",
+            kind: "soundtrack",
+            trackId: 1,
+            trackName: "Circle of Life",
+            trackNumber: 1,
+            artwork: "https://is1-ssl.mzstatic.com/image/thumb/Music118/v4/59/f2/cb/59f2cbb4-6800-2298-8de5-070caae3ec6a/00050086085873.rgb.jpg/100x100w.jpg",
+            previewUrl: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/53/bc/1c/53bc1c62-87a6-b255-7888-62e00972b18e/mzaf_1500676314994411111.plus.aac.p.m4a"
+        )
+        let viewControllerSpy = DetailsDisplayLogicSpy()
+        sut.viewController = viewControllerSpy
+        let response = Details.MediaPlayerResponse(media: media)
+
+        // When
+        sut.presentMediaPlayer(response: response)
+
+        // Then
+        XCTAssertTrue(viewControllerSpy.routeToMediaPlayerCalled)
     }
 }
